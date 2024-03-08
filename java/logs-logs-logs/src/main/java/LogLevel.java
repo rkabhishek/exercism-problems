386691892d1a491534bf8841328cdd3e76f7573b
Abhishek Kaushik <akaushik079@gmail.com>
2024-03-08 23:14:19 +0530

    Added solution for logs-logs-logs

--- java/logs-logs-logs/src/main/java/LogLevel.java
+++ java/logs-logs-logs/src/main/java/LogLevel.java
@@ -1,3 +1,22 @@
 public enum LogLevel {
-    // TODO: define members for each log level
+    UNKNOWN(0),
+    TRACE(1),
+    DEBUG(2),
+    INFO(4),
+    WARNING(5),
+    ERROR(6),
+    FATAL(42);
+
+    private final int level;
+
+    LogLevel(int level) {
+        this.level = level;
+    }
+
+    public int getLevel() {
+        return this.level;
+    }
 }
+
+
+
--- java/logs-logs-logs/src/main/java/LogLine.java
+++ java/logs-logs-logs/src/main/java/LogLine.java
@@ -1,13 +1,40 @@
 public class LogLine {
+    String logLine;
 
     public LogLine(String logLine) {
+        this.logLine = logLine;
     }
 
     public LogLevel getLogLevel() {
-        throw new UnsupportedOperationException("Please implement the getLogLevel() method");
+
+        String level = getLevelFromLog();
+        switch (level) {
+            case "TRC":
+                return LogLevel.TRACE;
+            case "DBG":
+                return LogLevel.DEBUG;
+            case "INF":
+                return LogLevel.INFO;
+            case "WRN":
+                return LogLevel.WARNING;
+            case "ERR":
+                return LogLevel.ERROR;
+            case "FTL":
+                return LogLevel.FATAL;
+            default:
+                return LogLevel.UNKNOWN;
+        }
     }
 
     public String getOutputForShortLog() {
-        throw new UnsupportedOperationException("Please implement the getOutputForShortLog() method");
+        return getLogLevel().getLevel() + ":" + getMessageFromLog();
+    }
+
+    private String getLevelFromLog() {
+        return logLine.substring(logLine.indexOf("[") + 1, logLine.indexOf("]"));
+    }
+
+    private String getMessageFromLog() {
+        return logLine.substring(logLine.indexOf("]:") + 2).trim();
     }
 }
