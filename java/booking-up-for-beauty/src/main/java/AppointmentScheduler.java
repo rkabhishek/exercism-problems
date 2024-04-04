3852090caf92fd38e065b66f1c7121c297e2a10e
Abhishek Kaushik <akaushik079@gmail.com>
2024-04-04 16:23:02 +0530

    Added solution for Booking Up for Beauty

---
+++ java/booking-up-for-beauty/src/main/java/AppointmentScheduler.java
@@ -0,0 +1,28 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+class AppointmentScheduler {
+    public LocalDateTime schedule(String appointmentDateDescription) {
+        DateTimeFormatter parser = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss");
+        return LocalDateTime.parse(appointmentDateDescription, parser);
+    }
+
+    public boolean hasPassed(LocalDateTime appointmentDate) {
+        return appointmentDate.isBefore(LocalDateTime.now());
+    }
+
+    public boolean isAfternoonAppointment(LocalDateTime appointmentDate) {
+        int hour = appointmentDate.getHour();
+        return hour >= 12 && hour < 18;
+    }
+
+    public String getDescription(LocalDateTime appointmentDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("'You have an appointment on' EEEE, MMMM d, yyyy, 'at' h:mm a'.'", Locale.ENGLISH);
+        return formatter.format(appointmentDate);
+    }
+
+    public LocalDate getAnniversaryDate() {
+        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.getAnniversaryDate() method");
+    }
+}
