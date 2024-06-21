b0fe08774634c236776832608a29c09a03a06dfa
Abhishek Kaushik <akaushik079@gmail.com>
2024-06-21 17:54:47 +0530

    Replaced manually counting digits with converting the integer to a string and counting its length

--- java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -15,17 +15,6 @@ class ArmstrongNumbers {
     }
 
     private int countDigits(int num){
-
-        if (num == 0)
-            return 1;
-
-        int count = 0;
-
-        while (num > 0) {
-            num /= 10;
-            count++;
-        }
-
-        return count;
+        return Integer.toString(num).length();
     }
 }
