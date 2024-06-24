28a247ca81158f24b45ffe2a27214ebba132df72
Abhishek Kaushik <akaushik079@gmail.com>
2024-06-24 16:45:33 +0530

    Refactor HighScores: instead of lazy construction, evaluating the values needed in the constructor

--- java/high-scores/src/main/java/HighScores.java
+++ java/high-scores/src/main/java/HighScores.java
@@ -5,12 +5,21 @@ import java.util.ArrayList;
 class HighScores {
 
     private final List<Integer> highScores;
+    private final Integer latestScore;
+    private final Integer highestScore;
+    private final List<Integer> maxTopThree;
 
     public HighScores(List<Integer> highScores) {
         if (highScores == null || highScores.isEmpty())
             throw new IllegalArgumentException("List of scores can not be null or empty");
 
         this.highScores = highScores;
+        latestScore = highScores.get(highScores.size() - 1);
+        highestScore = Collections.max(highScores);
+
+        List<Integer> copyOfScores = new ArrayList<Integer>(highScores);
+        Collections.sort(copyOfScores, Collections.reverseOrder());
+        maxTopThree = copyOfScores.subList(0, Math.min(3, copyOfScores.size()));
     }
 
     List<Integer> scores() {
@@ -18,17 +27,14 @@ class HighScores {
     }
 
     Integer latest() {
-        return highScores.get(highScores.size() - 1);
+        return latestScore;
     }
 
     Integer personalBest() {
-        return Collections.max(highScores);
+        return highestScore;
     }
 
     List<Integer> personalTopThree() {
-        List<Integer> copyOfScores = new ArrayList<Integer>(highScores);
-        Collections.sort(copyOfScores, Collections.reverseOrder());
-        List<Integer> maxTopThree = copyOfScores.subList(0, Math.min(3, copyOfScores.size()));
         return maxTopThree;
     }
 
